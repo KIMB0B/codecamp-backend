0147611e718950cc16c92a7eb065ee1840e5b12e
kimbob <axzsd96301@gmail.com>
2023-01-17 11:17:17 +0900

    day5-homework

---
+++ homework/day5/backend/swagger/config.js
@@ -0,0 +1,10 @@
+export const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: '4일차 과제',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./swagger/*.swagger.js'], // files containing annotations as above
+  };
