2ba1aa87b4b0e0c4ca4a5346e09a8d13eaeef4d1
kimbob <axzsd96301@gmail.com>
2023-01-21 21:23:04 +0900

    day7~8

---
+++ class/07/03-docker-with-express-package-json/backend/phone.js
@@ -0,0 +1,37 @@
+import coolsms from 'coolsms-node-sdk'
+import 'dotenv/config'
+
+export function checkValidationPhone(myphone) {
+    if (myphone.length !== 10 && myphone.length !== 11) {
+        console.log('에러 발생!!! 핸드폰 번호를 제대로 입력해 주세요!!!');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export function getToken(count) {
+    if (count === undefined) {
+        console.log('에러 발생!!! 갯수를 제대로 입력해 주세요!!!');
+        return;
+    } else if (count <= 0) {
+        console.log('에러 발생!!! 갯수가 너무 적습니다!!!');
+        return;
+    } else if (count > 10) {
+        console.log('에러 발생!!! 갯수가 너무 많습니다!!!');
+        return;
+    }
+    const result = String(Math.floor(Math.random() * 10 ** count)).padStart(count, '0');
+    return result;
+}
+
+export async function sendTokenToSMS(phoneNumber, tokenNumber) {
+    const messageService = new coolsms.default(process.env.SMS_KEY, process.env.SMS_SECRET);
+
+    const result = await messageService.sendOne({
+        to: phoneNumber,
+        from: process.env.SMS_SENDER,
+        text: `안녕하세요!! 요청하신 인증번호는 [${tokenNumber}] 입니다.`
+    });
+    console.log(result)
+}
